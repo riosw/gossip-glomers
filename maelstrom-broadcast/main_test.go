6199e9cca55b0993b427d9250329d5681bf1a526
Ong Rio Surya Wijaya <ongrios.wijaya@gmail.com>
2023-02-25 16:33:07 +0700

    Add test to unmarshal topology

--- maelstrom-broadcast/main_test.go
+++ maelstrom-broadcast/main_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"testing"
+)
 
 func TestTopologyType(t *testing.T) {
+	var topologyJSON = []byte(`{"type":"topology","topology":{"n0":["n3","n1"],"n1":["n4","n2","n0"],"n2":["n1"],"n3":["n0","n4"],"n4":["n1","n3"]},"msg_id":1}`)
+	var body map[string]any
+
+	if err := json.Unmarshal(topologyJSON, &body); err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	var topology = body["topology"].(map[string]interface{})
+
+	log.Default().Printf("Type of topology[\"n0\"] is: %T\n", topology["n0"])
+
+	n0Topology := topology["n0"].([]interface{})
+
+	s := make([]string, len(n0Topology))
+	for i, v := range n0Topology {
+		s[i] = fmt.Sprint(v)
+	}
+
+	var neighbors []string = s
+
+	log.Default().Println("Topology has been set: ", topology)
+	log.Default().Println("Neighbors has been set: ", neighbors)
+
+	for i, v := range neighbors {
+		log.Default().Printf("Neighbor #%d is: %s\n", i, v)
+	}
 
 }
