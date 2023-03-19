e11d205f4e1715ca9f777838f92b8922899acd80
Ong Rio Surya Wijaya <ongrios.wijaya@gmail.com>
2023-03-19 16:41:59 +0700

    Base g-counter

---
+++ maelstrom-g-counter/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	_ = maelstrom.NewSeqKV(n)
+
+	n.Handle("add", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		return n.Reply(msg, map[string]string{"type": "add_ok"})
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
