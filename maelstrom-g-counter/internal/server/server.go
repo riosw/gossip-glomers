e8ab0763952ccc4bbc92809499afd6aa29e22ead
Ong Rio Surya Wijaya <ongrios.wijaya@gmail.com>
2023-03-19 17:08:57 +0700

    Base Server & Add

---
+++ maelstrom-g-counter/internal/server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type Server struct {
+	Node *maelstrom.Node
+	kv   *maelstrom.KV
+}
+
+func New() *Server {
+	node := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(node)
+	return &Server{
+		Node: node,
+		kv:   kv,
+	}
+}
+
+func (s *Server) Add(delta int) error {
+	return nil
+}
--- maelstrom-g-counter/main.go
+++ maelstrom-g-counter/main.go
@@ -4,23 +4,30 @@ import (
 	"encoding/json"
 	"log"
 
+	"maelstrom-g-counter/internal/server"
+
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
-	n := maelstrom.NewNode()
-	_ = maelstrom.NewSeqKV(n)
+	s := server.New()
 
-	n.Handle("add", func(msg maelstrom.Message) error {
+	s.Node.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		return n.Reply(msg, map[string]string{"type": "add_ok"})
+		var delta int = body["delta"].(int)
+		err := s.Add(delta)
+		if err != nil {
+			return err
+		}
+
+		return s.Node.Reply(msg, map[string]string{"type": "add_ok"})
 	})
 
-	if err := n.Run(); err != nil {
+	if err := s.Node.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
